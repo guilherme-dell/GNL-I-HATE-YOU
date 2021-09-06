fcc431f69fe1a0c421fc4b629a026a41d6d908a1
Guilherme Dell <guilherme@>
2021-09-06 01:09:45 -0300

    new V0.7

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gnuncio- <gnuncio-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 10:02:13 by gnuncio-          #+#    #+#             */
-/*   Updated: 2021/09/06 00:29:36 by gnuncio-         ###   ########.fr       */
+/*   Updated: 2021/09/06 01:07:53 by gnuncio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,5 @@ char	*ft_strdup(const char *str);
 char	*ft_strchr(const char *string, int c);
 char	*ft_strjoin(char const *str1, char const *str2);
 char	*ft_substr(char const *str, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dest_len);
 
 #endif
